4f019f29e850b85e4905f4af73f5d2a788bfee10
Gantabya Kumar Bayda <gantabyakprottoy96@gmail.com>
2026-05-14 10:43:33 +0600

    To resolve promise, write code for catchAsync function

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,31 +2,32 @@ import type { Request, Response } from "express";
 
 import { AuthService } from "./auth.service";
 import { ApiResponse } from "../../utils/ApiResponse";
+import catchAsync from "../../utils/catchAsync";
 
 
-const login = async (req: Request, res: Response) => {
+const login = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.login(req.body);
 
   ApiResponse.success(res, result, "Login successful");
-};
+});
 
-const register = async (req: Request, res: Response) => {
+const register = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.register(req.body);
 
   ApiResponse.success(res, result, "Registration successful");
-};
+});
 
-const changePassword = async (req: Request, res: Response) => {
+const changePassword = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.changePassword(req.body);
 
   ApiResponse.success(res, result, "Password changed successfully");
-};
+});
 
-const forgotPassword = async (req: Request, res: Response) => {
+const forgotPassword = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.forgotPassword(req.body);
 
   ApiResponse.success(res, result, "Password reset email sent");
-};
+});
 
 export const AuthController = {
   login,
