d7c6320b5ba9be984a4040d70341464dcb9aa0ff
Gantabya Kumar Bayda <gantabyakprottoy96@gmail.com>
2026-05-19 19:51:19 +0600

    fixed minor issue in order controller

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -4,19 +4,16 @@ import { OrderService } from "./order.service";
 import { ApiResponse } from "../../../utils/ApiResponse";
 import catchAsync from "../../../utils/catchAsync";
 
-// Create Order
 const createOrder = catchAsync(async (req: Request, res: Response) => {
   const result = await OrderService.createOrder(req.body);
   ApiResponse.success(res, result, "Order created successfully");
 });
 
-// Get all orders
 const getOrders = catchAsync(async (req: Request, res: Response) => {
   const result = await OrderService.getOrders();
   ApiResponse.success(res, result, "Orders retrieved successfully");
 });
 
-// Get single order
 const getOrder = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
 
@@ -28,7 +25,6 @@ const getOrder = catchAsync(async (req: Request, res: Response) => {
   ApiResponse.success(res, result, "Order retrieved successfully");
 });
 
-// Update order
 const updateOrder = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
 
@@ -40,7 +36,6 @@ const updateOrder = catchAsync(async (req: Request, res: Response) => {
   ApiResponse.success(res, result, "Order updated successfully");
 });
 
-// Delete order
 const deleteOrder = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
 
